f56d192245ddafe4a86aa6491d304888b2fe0a0f
jonathanebrahimianls <jebrahimian@smu.edu>
2020-02-13 15:17:17 -0600

    uncommented input file to make gitactions work

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-     cout << "hello";
+     //cout << "hello";
      //DSVector<int> k;
 
 
     //k.append(2);
     //cout << k[0];
-//    cout << "Hello, World!" << endl;
-//    ofstream fout("output01.txt");
-//    fout << "Hello world!" << endl;
-//    fout.close();
+    cout << "Hello, World!" << endl;
+    ofstream fout("output01.txt");
+    fout << "Hello world!" << endl;
+    fout.close();
     return 0;
 }
